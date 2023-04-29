069bd4f5e2333f03e08abb8a26ebf696d9d1bfc7
AakashSingh28 <aakash.singh281991@gmail.com>
2023-04-29 13:58:01 +0530

    Updated the FunctionalProgramming01.java with lambda uses
    Signed-off-by: AakashSingh28 <aakash.singh281991@gmail.com>

--- src/main/java/com/programming/FunctionalProgramming01.java
+++ src/main/java/com/programming/FunctionalProgramming01.java
@@ -4,12 +4,39 @@ import java.util.List;
 
 public class FunctionalProgramming01 {
     public static void main(String[] args) {
-        printAllNumbersInListStructured(List.of(1,2,3,455,66,77,88,9,0));
+
+        List<Integer> numbers = List.of(1, 2, 3, 455, 66, 77, 88, 9, 0);
+        printAllNumbersInFunctional(numbers);
+        printEvenNumbersInFunctional(numbers);
     }
 
-    private static void printAllNumbersInListStructured(List<Integer> numbers) {
-        for (int num:numbers) {
-            System.out.println(" "+num);
-        }
+    private static void print(int number){
+        System.out.println(number);
+    }
+    private static void printAllNumbersInFunctional(List<Integer> numbers) {
+     //   numbers.stream().forEach(integer -> System.out.println(integer));
+        numbers.stream()
+                .forEach(FunctionalProgramming01::print);  // This is called Method Reference
+        // Or
+        numbers.stream()
+                .forEach(System.out :: println);
+    }
+    private static  boolean isEven(int number){
+         return number%2==0;
+    }
+
+    private static void printEvenNumbersInFunctional(List<Integer> numbers) {
+
+        numbers.stream()
+                //Filter - Only allow Even Numbers
+                .filter(FunctionalProgramming01:: isEven)
+                .forEach(System.out :: println);
+
+        // Or
+
+        numbers.stream()
+                // With lambda Expression
+                .filter(number -> number%2 == 0).forEach(System.out::println);
+
     }
 }
---
+++ src/main/java/com/programming/StructuredProgramming01.java
@@ -0,0 +1,15 @@
+package com.programming;
+
+import java.util.List;
+
+public class StructuredProgramming01 {
+    public static void main(String[] args) {
+        printAllNumbersInListStructured(List.of(1,2,3,455,66,77,88,9,0));
+    }
+
+    private static void printAllNumbersInListStructured(List<Integer> numbers) {
+        for (int num:numbers) {
+            System.out.println(" "+num);
+        }
+    }
+}
